c17fc74abe6c109d401d026aebfe3fcf070f1792
harish9444 <harishkidowboys@gmail.com>
2024-08-07 10:59:22 +0530

    "test"

---
+++ Dateandtime/src/datetime/datime.java
@@ -0,0 +1,17 @@
+package datetime;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class datime {
+	
+	public static void main(String[] args) {
+		LocalDate obj=LocalDate.now();
+		System.out.println(obj);
+		
+		LocalTime obj1=LocalTime.now();
+		System.out.println(obj1);
+		
+	}
+	
+
+}
